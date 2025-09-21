f91e8cf412f1e2699780bbbdb2ebf70bd8351a13
Justine Toong <programmerjustine@gmail.com>
2025-09-21 23:17:23 +0800

    Portfolio

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
     return (
         <>
             {/* <Headers></Headers> */}
-            <main className='flex flex-col items-center justify-center w-[85%] mx-auto'>
+            <main>
                 <HeroSection></HeroSection>
                 <AboutMe></AboutMe>
                 <Skills></Skills>
