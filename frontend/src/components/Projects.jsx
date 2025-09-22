03e8ec82b8ca33009e4bbf86d86b946b5a3a9143
Justine Toong <programmerjustine@gmail.com>
2025-09-22 20:36:40 +0800

    Portfolio

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import api from '../api/api';
+import Location from '../assets/location.png'
 
 const Projects = () => {
     const [projects, setProjects] = useState([]);
@@ -35,7 +36,7 @@ const Projects = () => {
                     return (
                         <div key={index} className='p-3 bg-[#f5f5f5] shadow-lg rounded-lg hover:scale-105 transition-transform duration-300'>
                             <img 
-                                src={project.image} 
+                                src={project.image ? project.image : Location} 
                                 alt={project.title} 
                                 className='h-[130px] w-full object-cover rounded-md mb-2' 
                             />
